695998a46c6832c998aa1fd6f40d306efd53ed18
Andrew <54324007+andrewten45@users.noreply.github.com>
2019-10-29 12:21:08 -0500

    Update identifier.cpp

--- identifier.cpp
+++ identifier.cpp
@@ -1,3 +1,8 @@
 // This program identifies whether an integer is a prime number or not.
 
 int userNum;
+
+cout << "Please enter a number: \n";
+cin >> userNum;
+
+if ((userNum //if it is prime.. what is prime?
