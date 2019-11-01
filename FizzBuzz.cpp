7522210e7613b802a27b7a6ebf302be38753de37
Andrew <54324007+andrewten45@users.noreply.github.com>
2019-11-01 18:44:11 -0500

    Update FizzBuzz.cpp
    Now it can say "FizzBuzz".

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -10,7 +10,11 @@ int main()
 
 	for (int i = 1; i <= 100; i++) {
 
-		if ((i % 3) == 0) {
+
+		if (((i % 3) == 0) && ((i % 5) == 0)) {
+		cout << "FizzBuzz\n";
+		}
+		else if ((i % 3) == 0) {
 			cout << "Fizz\n";
 		}
 		else if ((i % 5) == 0) {
