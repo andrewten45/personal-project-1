739b2e0d98b88552fc2dd402d3a333593ee22054
Andrew Tennant <andrewtennant2012@gmail.com>
2020-01-30 07:59:07 -0600

    Update kitchenHelper
    I made new functions for neat organization purposes, and arranged most functions in alphabetical order. I also renamed global variables such that each one now has a g_ before its name.

--- programs/favorites/kitchenHelper/kitchenHelper/kitchenHelper.cpp
+++ programs/favorites/kitchenHelper/kitchenHelper/kitchenHelper.cpp
@@ -5,88 +5,127 @@
 
 using namespace std;
 
-//FIXME: Find a way to not use global variables. Use different functions. Using return.
-int numCooking = 0;
-int numCooks = 0;
-int userTemp = 0;
-enum OvenState {OVEN_OFF, OVEN_ON_BAKE, OVEN_ON_BROIL};
-OvenState ovenStatus = OVEN_OFF;
+//FIXME: Find a way to not use global variables. Use different functions, also using return.
+int g_numCooking = 0;
+int g_numCooks = 0;
+int g_userTemp = 0;
+enum g_OvenState {OVEN_OFF, OVEN_ON_BAKE, OVEN_ON_BROIL};
+g_OvenState g_ovenStatus = OVEN_OFF;
 
-void kitchenDataEntry() {
-	string userString = "";
+void checkNumCooking() {
+	if (g_numCooking == 0) {
+		cout << "Nothing's cooking.\n";
+	}
+	else if (g_numCooking == 1) {
+		cout << "There's " << g_numCooking << " thing cooking.\n";
+	}
+	else {
+		cout << "There are " << g_numCooking << " things cooking.\n";
+	}
+}
 
-	cout << "You want to enter data.\n";
-	cout << "Input 1 to enter the amount of things cooking.\n";
-	cout << "Input 2 to enter the number of cooks working.\n";
-	cout << "Input 3 to enter oven settings.\n";
-	cout << endl;
-	cin >> userString;
+void checkNumCooks() {
+	if (g_numCooks == 0) {
+		cout << "Nobody's cooking.\n";
+	}
+	else if (g_numCooking == 1) {
+		cout << "There's " << g_numCooks << " cook working.\n";
+	}
+	else {
+		cout << "There are " << g_numCooks << " cooks working.\n";
+	}
+}
 
-	if (userString == "1") {
-		cout << "Please enter the number of things cooking:\n";
-		cin >> numCooking;
-
-		if (numCooking == 0) {
-			cout << "Nothing's cooking.\n";
-		}
-		else if (numCooking == 1) {
-			cout << "There's " << numCooking << " thing cooking.\n";
+void checkOvenStatus() {
+	if (g_ovenStatus == OVEN_OFF) {
+		cout << "Oven is off.\n";
+	}
+	else if (g_ovenStatus == OVEN_ON_BAKE) {
+		cout << "Oven is on, and set to bake.\n";
+		if (g_userTemp == 1) {
+			cout << "The oven is set to " << g_userTemp << " degree Fahrenheit.\n";
 		}
 		else {
-			cout << "There are " << numCooking << " things cooking.\n";
+			cout << "The oven is set to " << g_userTemp << " degrees Fahrenheit.\n";
+		}
+	}
+	else if (g_ovenStatus == OVEN_ON_BROIL) {
+		cout << "Oven is on, and set to broil.\n";
+		if (g_userTemp == 1) {
+			cout << "The oven is set to " << g_userTemp << " degree Fahrenheit.\n";
+		}
+		else {
+			cout << "The oven is set to " << g_userTemp << " degrees Fahrenheit.\n";
+		}
+	}
+}
+
+void enterNumCooking() {
+	cout << "Please enter the number of items cooking:\n";
+	cin >> g_numCooking;
+
+	if (g_numCooking == 0) {
+		cout << "Nothing's cooking.\n";
+	}
+	else if (g_numCooking == 1) {
+		cout << "There's " << g_numCooking << " thing cooking.\n";
+	}
+	else {
+		cout << "There are " << g_numCooking << " things cooking.\n";
+	}
+}
+
+void enterNumCooks() {
+	cout << "Please enter the number of cooks working:\n";
+	cin >> g_numCooks;
+
+	if (g_numCooks == 0) {
+		cout << "Nobody's cooking.\n";
+	}
+	else if (g_numCooking == 1) {
+		cout << "There's " << g_numCooks << " cooks working.\n";
+	}
+	else {
+		cout << "There are " << g_numCooks << " cooks working.\n";
+	}
+}
+
+void enterOvenStatus() {
+	string userString;
+
+	cout << "Please input 0 to turn the oven off and 1 or 2 to turn on bake or broil, respectively.\n";
+	cin >> userString;
+
+	if (userString == "0") {
+		g_ovenStatus = OVEN_OFF;
+		cout << "Oven is off.\n";
+	}
+	else if (userString == "1") {
+		g_ovenStatus = OVEN_ON_BAKE;
+		cout << "Oven is on, and set to bake.\n";
+
+		cout << "What temperature (F)?\n";
+		cin >> g_userTemp;
+		if (g_userTemp == 1) {
+			cout << "The oven is set to " << g_userTemp << " degree Fahrenheit.\n";
+		}
+		else {
+			cout << "The oven is set to " << g_userTemp << " degrees Fahrenheit.\n";
 		}
 	}
 	else if (userString == "2") {
-		cout << "Please enter the number of cooks working:\n";
-		cin >> numCooks;
+		g_ovenStatus = OVEN_ON_BROIL;
+		cout << "Oven is on, and set to broil.\n";
 
-		if (numCooks == 0) {
-			cout << "Nobody's cooking.\n";
-		}
-		else if (numCooking == 1) {
-			cout << "There's " << numCooks << " cooks working.\n";
+		cout << "What temperature (F)?\n";
+		cin >> g_userTemp;
+		if (g_userTemp == 1) {
+			cout << "The oven is set to " << g_userTemp << " degree Fahrenheit.\n";
 		}
 		else {
-			cout << "There are " << numCooks << " cooks working.\n";
+			cout << "The oven is set to " << g_userTemp << " degrees Fahrenheit.\n";
 		}
 	}
-	else if (userString == "3") {
-		cout << "Please input 0 to turn the oven off and 1 or 2 to turn on bake or broil, respectively.\n";
-		cin >> userString;
-
-		if (userString == "0") {
-			ovenStatus = OVEN_OFF;
-			cout << "Oven is off.\n";
-		}
-		else if (userString == "1") {
-			ovenStatus = OVEN_ON_BAKE;
-			cout << "Oven is on, and set to bake.\n";
-
-			cout << "What temperature (F)?\n";
-			cin >> userTemp;
-			if (userTemp == 1) {
-				cout << "The oven is set to " << userTemp << " degree Fahrenheit.\n";
-			}
-			else {
-				cout << "The oven is set to " << userTemp << " degrees Fahrenheit.\n";
-			}
-		}
-		else if (userString == "2") {
-			ovenStatus = OVEN_ON_BROIL;
-			cout << "Oven is on, and set to broil.\n";
-
-			cout << "What temperature (F)?\n";
-			cin >> userTemp;
-			if (userTemp == 1) {
-				cout << "The oven is set to " << userTemp << " degree Fahrenheit.\n";
-			}
-			else {
-				cout << "The oven is set to " << userTemp << " degrees Fahrenheit.\n";
-			}
-		}
-	}
-
-	cout << endl;
 }
 
 void kitchenCheckStatus() {
@@ -101,49 +140,36 @@ void kitchenCheckStatus() {
 	cin >> userString;
 
 	if (userString == "1") {
-		if (numCooking == 0) {
-			cout << "Nothing's cooking.\n";
-		}
-		else if (numCooking == 1) {
-			cout << "There's " << numCooking << " thing cooking.\n";
-		}
-		else {
-			cout << "There are " << numCooking << " things cooking.\n";
-		}
+		checkNumCooking();
 	}
 	else if (userString == "2") {
-		if (numCooks == 0) {
-			cout << "Nobody's cooking.\n";
-		}
-		else if (numCooking == 1) {
-			cout << "There's " << numCooks << " cook working.\n";
-		}
-		else {
-			cout << "There are " << numCooks << " cooks working.\n";
-		}
+		checkNumCooks();
 	}
 	else if (userString == "3") {
-		if (ovenStatus == OVEN_OFF) {
-			cout << "Oven is off.\n";
-		}
-		else if (ovenStatus == OVEN_ON_BAKE) {
-			cout << "Oven is on, and set to bake.\n";
-			if (userTemp == 1) {
-				cout << "The oven is set to " << userTemp << " degree Fahrenheit.\n";
-			}
-			else {
-				cout << "The oven is set to " << userTemp << " degrees Fahrenheit.\n";
-			}
-		}
-		else if (ovenStatus == OVEN_ON_BROIL) {
-			cout << "Oven is on, and set to broil.\n";
-			if (userTemp == 1) {
-				cout << "The oven is set to " << userTemp << " degree Fahrenheit.\n";
-			}
-			else {
-				cout << "The oven is set to " << userTemp << " degrees Fahrenheit.\n";
-			}
-		}
+		checkOvenStatus();
+	}
+
+	cout << endl;
+}
+
+void kitchenDataEntry() {
+	string userString = "";
+
+	cout << "You want to enter data.\n";
+	cout << "Input 1 to enter the amount of things cooking.\n";
+	cout << "Input 2 to enter the number of cooks working.\n";
+	cout << "Input 3 to enter oven settings.\n";
+	cout << endl;
+	cin >> userString;
+
+	if (userString == "1") {
+		enterNumCooking();
+	}
+	else if (userString == "2") {
+		enterNumCooks();
+	}
+	else if (userString == "3") {
+		enterOvenStatus();
 	}
 
 	cout << endl;
