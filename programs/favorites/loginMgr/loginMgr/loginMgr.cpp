66f046506a6d26e752a156a31cb8c536bfcf6075
Andrew Tennant <andrewtennant2012@gmail.com>
2020-02-11 08:07:22 -0600

    Update

--- programs/favorites/loginMgr/loginMgr/loginMgr.cpp
+++ programs/favorites/loginMgr/loginMgr/loginMgr.cpp
@@ -11,37 +11,40 @@ struct login {
 	string userPass = "";
 };
 
-void loginMgr() {
-	string userChoice;
-
-	while (true) {
-		cout << "Welcome to the Login manager by Andrew T. You can save logins using this program. \n\n";
-		cout << "Enter 1 to save a new login.\n";
-		cout << "Enter 2 to view all of your logins.\n";
-		cout << "Enter any other input to quit.\n";
-		cout << endl;
-
-		cin >> userChoice;
-
-		if (userChoice == "1") {
-			loginSave();
-		}
-		if (userChoice == "2") {
-			loginView();
-		}
-		else {
-			break;
-		}
-	}
-}
-
-//FIXME: Why is the identifier not found?
 void loginSave() {
 	cout << "You have chosen to save a login.\n";
+	//TODO: Enter code to save logins using the structure.
 }
 
 void loginView() {
 	cout << "You have chosen to view logins.\n";
+	//TODO: Enter code to view logins.
+}
+
+void loginMgr() {
+	string userChoice;
+
+	cout << "Welcome to the Login manager by Andrew T. You can save logins using this program. \n\n";
+
+	while (true) {
+		cout << "Enter 0 to quit.\n";
+		cout << "Enter 1 to save a new login.\n";
+		cout << "Enter 2 to view all of your logins.\n";
+		cout << "Enter any other input to repeat the prompt.\n";
+		cout << endl;
+
+		cin >> userChoice;
+
+		if (userChoice == "0") {
+			break;
+		}
+		else if (userChoice == "1") {
+			loginSave();
+		}
+		else if (userChoice == "2") {
+			loginView();
+		}
+	}
 }
 
 int main()
