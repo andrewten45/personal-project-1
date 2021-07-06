89e15a5ca7dc297b14c1010b3b3fcae507d03592
Andrew <andrewtennant2012@gmail.com>
2021-07-06 14:01:36 -0500

    Update test_numRandom.rb
    New prompt functionality introduced to the test ruby file.

--- snippets/numRandom/test/test_numRandom.rb
+++ snippets/numRandom/test/test_numRandom.rb
@@ -1,9 +1,20 @@
 #Test site
 
-class MyClass
-        def self.generic_method
-                puts 'Random number, got: '
+class GenNumRandom
+        def self.prompt
+                puts "How many times to generate a number?"
+                
+                numIterations = gets.to_i
+                if numIterations > 0
+                        puts "valid response"
+                else
+                        puts "invalid response"
+                end
+        end
+        def self.gen
+                puts "Random number, got: "
                 puts rand(0 .. 10)
         end
 end
-MyClass.generic_method
+
+GenNumRandom.prompt
