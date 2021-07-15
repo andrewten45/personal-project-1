e09b7aeac303c2d0282b63358edc36a753518105
Andrew <andrewtennant2012@gmail.com>
2021-07-15 13:33:35 -0500

    Update test_numRandom.rb
    Added generation # to help navigate the results.

--- snippets/numRandom/test/test_numRandom.rb
+++ snippets/numRandom/test/test_numRandom.rb
@@ -6,25 +6,33 @@ class GenNumRandom
                 puts "How many times to generate a number?"
                 
                 numIterations = gets.to_i
+                numGens = 0
                 if numIterations == 0
                         puts "Ok, no times."
-                elsif numIterations == 1
-                        puts "Ok, " + numIterations.to_s + " time."
+                elsif numIterations >= 1
+                        if numIterations == 1
+                                puts "Ok, " + numIterations.to_s + " time."
 
-                        self.gen
-                elsif numIterations > 1
-                        puts "Ok, " + numIterations.to_s + " times."
-
-                        for i in 0..numIterations - 1
                                 self.gen
+                                numGens = numGens + 1
+                                puts "That was generation #" + numGens.to_s
+                                #The big question: What is a good format for succinctly informing the user of both generation # and # generated?
+                        else
+                                puts "Ok, " + numIterations.to_s + " times."
+
+                                for i in 0..numIterations - 1
+                                        self.gen
+                                        numGens = numGens + 1
+                                        puts "That was generation #" + numGens.to_s
+                                end
                         end
                 else
                         puts "Invalid input."
                 end
         end
         def self.gen
-                puts "Random number, got: "
-                puts rand(0 .. 10)
+                puts "Gererating a random number between up to 100, got: "
+                puts rand(0 .. 100)
         end
 end
 
