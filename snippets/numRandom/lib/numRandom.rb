66ee6201c1ecb2e20f1bd54f73e953fa494fd37c
Andrew Tennant <andrewtennant2012@gmail.com>
2021-09-08 16:57:08 -0500

    Re-organization
    Instead of making "test versions" in the _test file, I learned it should be used for small unit tests. I moved the test version to the main file.

--- snippets/numRandom/lib/numRandom.rb
+++ snippets/numRandom/lib/numRandom.rb
@@ -1,38 +1,34 @@
 #Random number generator class
 
 class GenNumRandom
-        def self.prompt
-                puts "How many times to generate a number?"
-                
-                numIterations = gets.to_i
-                puts "\n"
-                numGens = 0
-                if numIterations == 0
-                        puts "Ok, no times."
-                elsif numIterations >= 1
-                        if numIterations == 1
-                                puts "Ok, " + numIterations.to_s + " time.\n\n"
-
-                                self.gen
-                                numGens = numGens + 1
-                                puts "Generation #" + numGens.to_s + "\n\n"
-                        else
-                                puts "Ok, " + numIterations.to_s + " times.\n\n"
-
-                                for i in 0..numIterations - 1
-                                        self.gen
-                                        numGens = numGens + 1
-                                        puts "Generation #" + numGens.to_s + "\n\n"
-                                end
-                        end
-                else
-                        puts "Invalid input."
-                end
-        end
         def self.gen
-                puts "Random number between 0 and 100. Result:"
-                puts rand(0 .. 100)
+                #Step 1: Gather user's input
+                puts "Minimum number:"
+                numMin = gets.to_i
+                puts "\n"
+
+                puts "Maximum number:"
+                numMax = gets.to_i
+                puts "\n"
+
+                puts "How many times to generate a number?"
+                numIter = gets.to_i
+                puts "\n"
+
+                #Step 2: Display input
+                puts "You have set these parameters:"
+                puts "Minimum number: " + numMin.to_s
+                puts "Maximum number: " + numMax.to_s
+                puts "Iterations: " + numIter.to_s
+                puts "\n"
+
+                #Step 3: Random number generation
+                for i in 0..numIter - 1
+                        puts rand(numMin .. numMax)
+                end
+
+                puts "Done. Program will now close."
         end
 end
 
-GenNumRandom.prompt
+GenNumRandom.gen
