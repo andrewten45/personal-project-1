0b7aed65f1dd79243b1f81a03b88310064ae568a
andrew <54324007+andrewten45@users.noreply.github.com>
2023-10-02 09:37:00 -0500

    Update test_numRandom.rb

--- snippets/numRandom/test/test_numRandom.rb
+++ snippets/numRandom/test/test_numRandom.rb
@@ -1,2 +1,11 @@
 #Test site for random number generator class
-#For small unit tests
+#Unit testing in the traditional sense is difficult; opting for testing using generated values
+
+#require numRandom.rb
+require 'csv'
+
+csv_string = CSV.generate do |csv|
+  #csv << ["row", "of", "CSV", "data"]
+  #csv << ["another", "row"]
+  # ...
+end
