361fa9dafa85821568a7d0e0b1370faae06541f8
andrew <54324007+andrewten45@users.noreply.github.com>
2023-11-03 09:20:35 -0500

    Add timeConvert.rb

---
+++ snippets/timeConvert/timeConvert.rb
@@ -0,0 +1,10 @@
+#timeConvert.rb - convert given military time input to UTC & return value.
+
+initialTime = '0900'
+
+def convert_time # 0900 CST -> 1400 UTC / Add 5 hours
+    # Convert to numerical value
+    # Add 5 hours
+    # Convert back to string
+    # Return value
+end
