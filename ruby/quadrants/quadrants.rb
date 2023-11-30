201bfee05f60641492966d80104544953c72160e
andrew <54324007+andrewten45@users.noreply.github.com>
2023-11-30 10:10:12 -0600

    Refactored for readability

--- ruby/quadrants/quadrants.rb
+++ ruby/quadrants/quadrants.rb
@@ -1,16 +1,13 @@
 def quadrant(x, y)
-    if x > 0
-      if y > 0
-        return 1
-        end
-      elsif y < 0
-      return 3
-      end
-    if x < 0
-      if y > 0
-        return 2
-        end
-      elsif y < 0
-      return 4
-      end
+    case [x, y]
+    in [0.., 0..] then 1
+    in [..0, 0..] then 2
+    in [..0, ..0] then 3
+    in [0.., ..0] then 4
   end
+end
+
+puts quadrant(1, 1) # Should output "1"
+puts quadrant(-1, 1) # Should output "2"
+puts quadrant(-1, -1) # Should output "3"
+puts quadrant(1, -1) # Should output "4"
